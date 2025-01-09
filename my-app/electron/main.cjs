b749fc58de49dc8415864e7b6543c859ef515e68
JosueNolasco2001 <josuenolascoaguilera@gmail.com>
2025-01-09 15:08:19 -0600

    agregando camios de estilo

--- my-app/electron/main.cjs
+++ my-app/electron/main.cjs
@@ -3,8 +3,7 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+      fullscreen: true, // Activa pantalla completa
     webPreferences: {
       preload: path.join(__dirname, './preload.cjs'),
       nodeIntegration: false,
