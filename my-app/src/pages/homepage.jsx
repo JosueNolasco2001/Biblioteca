6b231ba831af468139bc151d71a96f3ce8b9bb43
JosueNolasco2001 <josuenolascoaguilera@gmail.com>
2025-01-09 21:27:44 -0600

    agregando estilos

--- my-app/src/pages/homepage.jsx
+++ my-app/src/pages/homepage.jsx
@@ -5,11 +5,11 @@ import './styles.scss'
 function homepage() {
     return (
 
-        <main class="relative h-screen bg-transparent dark:bg-gray-800">
+        <main class="relative h-screen w-sc bg-transparent ">
             <header class="z-30 flex items-center w-full h-24 sm:h-32">
-                <div class="container flex items-center justify-between px-6 mx-auto">
+                <div class="container min-w-full flex items-center justify-between ">
                     <div class="text-3xl font-black text-gray-800 uppercase dark:text-white">
-                    <img class="w-[300px] h-" alt="hero" src={imgSenacit} />
+                        <img class="w-[300px] h-" alt="hero" src={imgSenacit} />
 
                     </div>
                     <div class="flex items-center">
@@ -48,30 +48,69 @@ function homepage() {
                     </div>
                     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                         <h1 class="text-6xl sm:text-8xl font-extrabold text-gray-900 uppercase tracking-wide mb-6">
-                            Biblioteca 
+                            Biblioteca
                         </h1>
                         <h1 class="text-6xl sm:text-8xl font-extrabold text-[#00d9d5] uppercase tracking-wide mb-6">
-                        Virtual
+                            Virtual
                         </h1>
 
-                        <p class="mb-8 leading-relaxed font-sans text-[#191b1bbb] bg-[#00d9d5f3] p-6 rounded-md text-[15px]  ">
 
-                            En el marco del compromiso de la administración de la Presidenta Xiomara Castro con la educación y el desarrollo, hemos creado la Biblioteca Virtual de SENACIT, una plataforma que pone al alcance de todos los hondureños una amplia colección de recursos educativos, científicos y culturales.
 
-                            Nuestra misión es democratizar el acceso al conocimiento, fortaleciendo la investigación, la innovación y el aprendizaje a lo largo de la vida. Aquí podrás encontrar libros, revistas científicas, artículos académicos, contenido multimedia y mucho más, todo organizado para facilitar el aprendizaje y la exploración.</p>
-                        <div className='min-w-full'>
+                        <div className="flex-shrink-0  relative overflow-hidden bg-[#6446F6] rounded-lg  shadow-lg w-full">
+                            <svg
+                                className="absolute bottom-0 left-0 mb-8"
+                                viewBox="0 0 375 283"
+                                fill="none"
+                                style={{ transform: 'scale(1.5)', opacity: 0.1 }}
+                            >
+                                <rect
+                                    x="159.52"
+                                    y="175"
+                                    width="152"
+                                    height="152"
+                                    rx="8"
+                                    transform="rotate(-45 159.52 175)"
+                                    fill="white"
+                                />
+                                <rect
+                                    y="107.48"
+                                    width="152"
+                                    height="152"
+                                    rx="8"
+                                    transform="rotate(-45 0 107.48)"
+                                    fill="white"
+                                />
+                            </svg>
+                            <div className="relative  px-10 flex items-center justify-center">
+                                <div
+                                    className="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3"
+                                    style={{
+                                        background: 'radial-gradient(black, transparent 60%)',
+                                        transform: 'rotate3d(0, 0, 1, 20deg) scale3d(1, 0.6, 1)',
+                                        opacity: 0.2,
+                                    }}
+                                ></div>
+
+                            </div>
+                            <div className="relative text-white px-6 pb-6 mt-6">
+
+                                <div className="flex justify-between">
+
+                                    En el marco del compromiso de la administración de la Presidenta Xiomara Castro con la educación y el desarrollo, hemos creado la Biblioteca Virtual de SENACIT, una plataforma que pone al alcance de todos los hondureños una amplia colección de recursos educativos, científicos y culturales.
+
+                                    Nuestra misión es democratizar el acceso al conocimiento, fortaleciendo la investigación, la innovación y el aprendizaje a lo largo de la vida. Aquí podrás encontrar libros, revistas científicas, artículos académicos, contenido multimedia y mucho más, todo organizado para facilitar el aprendizaje y la exploración.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='min-w-full mt-9'>
 
                             <center>
 
-                                <section className="portfolio-experiment 0">
-                                    <a>
-                                        <span class="text px-20 text-white">Iniciar</span>
-                                        <span class="line -right"></span>
-                                        <span class="line -top"></span>
-                                        <span class="line -left"></span>
-                                        <span class="line -bottom"></span>
-                                    </a>
-                                </section>
+                            <div class="container ">
+  <button class="btn" href="#">Continuar</button>
+</div>
+
 
                             </center>
                         </div>
